6f970c49b2f4fbeb8a172f3a7938cce2cf818c04
Alexander Perez Oliva <alexanderperez900.1@gmail.com>
2022-11-10 18:21:32 -0600

    Initial Commit

---
+++ src/main/java/dmacc/FamilyRentalServiceApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyRentalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyRentalServiceApplication.class, args);
+	}
+
+}
